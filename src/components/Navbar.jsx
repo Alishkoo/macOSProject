619f11fbcc76490c84aac2c96ee69ed69a5491a8
Alishkoo <baisholanov.alibek@gmail.com>
2025-12-10 00:22:40 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { navLinks } from "#constants";
+import { navIcons } from "#constants";
+import dayjs from "dayjs";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Alibek's Project</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time> {dayjs().format("MMMM D, YYYY")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
